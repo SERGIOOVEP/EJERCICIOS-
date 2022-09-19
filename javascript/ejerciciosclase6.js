02c7906aa0e1ff3e2aacd24d5b38bb3ea6da911a
SERGIOOVEP <sergio.ovejeropascual@gmail.com>
2022-09-19 19:38:00 +0200

    falta el 2 y el 6

--- javascript/ejerciciosclase6.js
+++ javascript/ejerciciosclase6.js
@@ -1,11 +1,11 @@
 
-/*Solicita al usuario tres números enteros e indícale cuál es el menor.*/
+/*Solicita al usuario tres números enteros e indícale cuál es el menor.
 alert ("Inserta un número entero")
 var a =prompt (a)
 var b =prompt (b)
 var c =prompt (c)
 
-
+/*
 if (a <= b && a <= c) {
     alert (a);
 } else if (b <= a && b <= c) {
@@ -14,6 +14,60 @@ if (a <= b && a <= c) {
     alert (c);
 }
 
-/*Solicita al usuario una frase y una letra y muestra la cantidad de veces que aparece la letra en la frase.*/
+
+/*Solicita al usuario una frase y una letra y muestra la cantidad de veces que aparece la letra en la frase.
+
+alert ("Introduce una frase")
+var a = prompt (a)
+alert ("introduce una letra")
+var b = prompt (b)
+*/
+
+/*Suma o resta (según elija el usuario) dos números reales.
+var a = prompt ("intoduce un número")
+var b = prompt ("introduce otro número")
+var c = parseInt(a) + parseInt(b)
+alert (c);
+
+Almacena en dos variables datos de validación (usuario y contraseña) correctos y permite que el usuario valide (dispone de 3 intentos)*/
+
+/*
+var a = prompt ("Introduzca usuario")
+var b = prompt ("Introduzca contraseña")
+
+if (a=="sergio" && b== "gol") {
+    alert("Bienvenido");
+} else {
+    alert("incorrecto");
+}
+
+
+Solicita al usuario una letra, si inserta una a muestra el número 7, si es una b, el 9, 
+si es una c el 101 y si no es ninguno de los tres, indícale que se ha equivocado de letra 
+
+alert ("Inserta una letra")
+
+var n = prompt (n);
+
+if (n =="a") {
+    alert (7);
+} else if (n =="b") {
+    alert (9);
+} else if (n =="c") {
+    alert (101);
+} else {
+    alert ("Se ha equivocado de letra");
+}
+
+
+
+Ordena alfabéticamente un array con 7 palabras, puedes usar el algoritmo de la burbuja  */
+
+
+
+let palabras = ["futbol","baloncesto","tenis","natacion","padel","atletismo","golf"];
+palabras.sort((a,b) {
+
+   
 
 
